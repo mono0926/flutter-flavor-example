d3b3c6ec1bc0e333aefa3b5c1fd44de0445e2a38
Masayuki Ono <mono0926@gmail.com>
2020-05-26 10:33:53 +0900

    Fix iOS Firebase Flavor

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,36 +13,7 @@ import Firebase
     let flavor = Flavor.current
     print("buildMode: \(buildMode), flavor: \(flavor)")
 
-    configureFirebase()
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
-
-extension Flavor {
-    var firebaseOptions: FirebaseOptions {
-        let filename = { () -> String in
-            let base = "GoogleService-Info"
-            switch Flavor.current {
-            case .development: return "\(base)-Development"
-            case .staging: return "\(base)-Staging"
-            case .production: return "\(base)-Production"
-            }
-        }()
-        let path = Bundle.main.path(forResource: filename, ofType: "plist")!
-        return FirebaseOptions(contentsOfFile: path)!
-    }
-}
-
-private func configureFirebase() {
-    let firOptions = Flavor.current.firebaseOptions
-    FirebaseApp.configure(options: firOptions)
-
-    // FirestoreでTimestamp型を使うように
-    // Flutter側でやるのでコメントアウト
-//    let db = Firestore.firestore()
-//    let settings = db.settings
-//    settings.areTimestampsInSnapshotsEnabled = true
-//    db.settings = settings
 }
